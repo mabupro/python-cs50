f585defb772b24a05104e2196992e48d46333a56
mabupro <mabupro787@gmail.com>
2024-03-13 13:09:43 +0900

    finished/lecture0

--- calculator.py
+++ calculator.py
@@ -1,20 +1,8 @@
-# x = 1
-# x = int(input("What's x? "))
-x = float(input("What's x? "))
+def main():
+    x = int(input("What's x?"))
+    print("x squared is", square(x))
 
-# y = 2
-# y = int(input("What's y? "))
-y = float(input("What's y? "))
+def square(n):
+    return n * n
 
-# z = x + y
-# z = int(x) + int(y)
-# z = round(x + y)
-
-z = round(x / y, 2)
-
-# print(z)
-# print(x + y)
-# print(z)
-# print(f"{z:,}")
-print(f"{z:.2f}")
-
+main()
